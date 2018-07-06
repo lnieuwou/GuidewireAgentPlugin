f53275ddfb7c4a971239a6e9708b58e4f4994ca7
louis.nieuwoudt <louis.nieuwoudt@googlemail.com>
2018-07-06 14:47:15 +0100

    First commit

--- src/main/resources/exportToPem.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# used pw AppD123
-keytool -importkeystore -srckeystore GuidewireAgentPlugin.keystore -destkeystore keys.p12 -srcstoretype jks -deststoretype pkcs12
-
-openssl pkcs12 -in keys.p12 -out keys.pem
